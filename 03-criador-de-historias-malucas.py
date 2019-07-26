be1aed45a83d4307f48f2118703fc7386b2f2a82
Maike Negreiros <maike_jbe@hotmail.com>
2019-07-25 23:10:33 -0300

    Criado criador de historias malucas

---
+++ 03-criador-de-historias-malucas.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+print("****** Criador de histórias malucas ******")
+
+person = input('digite o nome de uma pessoa: ')
+famous_person = input('digite o nome de uma pessoa famosa: ')
+animal = input('digite o nome de um animal: ')
+adjective = input('digite um adjetivo: ')
+place = input('digite um nomem de um lugar: ')
+
+
+
+str = f'{famous_person} uma vez disse: Como pode de uma cabra nascer um bode? Todos sabem que quando {person} for à {place}, qualquer {animal} com fome pode ser {adjective}'
+
+
+print(str)
